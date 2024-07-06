e7997c9664119cb409a1c8eec8af03ded3f09428
jf129 <159708943+jf129@users.noreply.github.com>
2024-07-06 15:53:21 +0100

    news info

--- app/(tabs)/news.jsx
+++ app/(tabs)/news.jsx
@@ -12,14 +12,17 @@ const News = () => {
             <View className="relative mt-5">
                 <Image className="justify-center align-middle" source={images.news1} />
                 <Text className="text-white justify-center text-center text-lg font-pbold">Acteon to create Kilcardine digital twin</Text>
+                <Text className="text-white justify-center text-left text-sm font-pregular ">Acteon has been commissioned to create a digital twin of the 50MW Kiincardine offshore wind farm that will help optimise future imspection and surcey activities</Text>
             </View>
             <View className="relative mt-5 align-middle">
                 <Image className="justify-center align-middle" source={images.news2} />
                 <Text className="text-white justify-center text-center text-lg font-pbold">Moray West offshore wind farm delivers first power</Text>
+                <Text className="text-white justify-center text-left text-sm font-pregular ">Ocean Winds has delivered first power from its 882MW Moray West offshore wind farm</Text>
             </View>
             <View className="relative mt-5 align-middle">
                 <Image className="justify-center align-middle" source={images.news3} />
                 <Text className="text-white justify-center text-center text-lg font-pbold">The Scottish tech firm developiing fully automated offshore search and rescue</Text>
+                <Text className="text-white justify-center text-left text-sm font-pregular ">Scottish Technology firm Zelim has a simple mission statement at its heart, to "make unmanned search and rescue the  industry standard".</Text>
             </View>
         </ScrollView>
     </SafeAreaView>
