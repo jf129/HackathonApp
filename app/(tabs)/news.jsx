8a12409cdb0a9f82d5bc210f0154a5474b79fa65
jf129 <159708943+jf129@users.noreply.github.com>
2024-07-06 15:49:42 +0100

    add news

--- app/(tabs)/news.jsx
+++ app/(tabs)/news.jsx
@@ -1,13 +1,26 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
 
 const News = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       {/* Allow to user to scroll news */}
         <ScrollView>
-          <Text className="text-secondary-100 justify-center text-center text-xl font-pextrabold">News</Text>
+        <Text className="text-secondary-100 justify-center text-center text-xl font-pextrabold">News</Text>
+            <View className="relative mt-5">
+                <Image className="justify-center align-middle" source={images.news1} />
+                <Text className="text-white justify-center text-center text-lg font-pbold">Acteon to create Kilcardine digital twin</Text>
+            </View>
+            <View className="relative mt-5 align-middle">
+                <Image className="justify-center align-middle" source={images.news2} />
+                <Text className="text-white justify-center text-center text-lg font-pbold">Moray West offshore wind farm delivers first power</Text>
+            </View>
+            <View className="relative mt-5 align-middle">
+                <Image className="justify-center align-middle" source={images.news3} />
+                <Text className="text-white justify-center text-center text-lg font-pbold">The Scottish tech firm developiing fully automated offshore search and rescue</Text>
+            </View>
         </ScrollView>
     </SafeAreaView>
   )
--- constants/images.js
+++ constants/images.js
@@ -5,7 +5,10 @@ import path from "../assets/images/path.png";
 import bioHot from "../assets/images/bioHot.png";
 import geospatial from "../assets/images/geospatial.png";
 import usage from "../assets/images/usage.png";
+import news1 from "../assets/images/news1.jpeg";
+import news2 from "../assets/images/news2.jpeg";
+import news3 from "../assets/images/news3.jpeg";
 
 // Exports images that are used
 
-export default { cards, logoSmall, logo, path, bioHot, geospatial, usage };
+export default { cards, logoSmall, logo, path, bioHot, geospatial, usage , news1, news2, news3 };
